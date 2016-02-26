ddbf7eac360a34c9364df79a35d7c98ea53aa31f
Florian Biermann <fbie@itu.dk>
2016-02-26 11:22:33 +0100

    implement horizontal and vertical array-catenation

--- RadixTree/Utils.fs
+++ RadixTree/Utils.fs
@@ -45,4 +45,16 @@ module Array2D =
         height arr = 1
 
     let inline isSingleton arr =
-        width arr = 1 && height arr = 1
+        Array2D.length1 arr = 1 && Array2D.length2 arr = 1
+
+    let hcat left right =
+        let l1 = min (Array2D.length1 left) (Array2D.length1 right)
+        let l2 = Array2D.length2 left + Array2D.length2 right
+        let l2l = Array2D.length2 left
+        Array2D.init l1 l2 (fun i j -> if j < l2l then left.[i, j] else right.[i, j - l2l])
+
+    let vcat left right =
+        let l1 = Array2D.length1 left + Array2D.length1 right
+        let l2 = min (Array2D.length2 left) (Array2D.length2 right)
+        let l1l = Array2D.length1 left
+        Array2D.init l1 l2 (fun i j -> if i < l1l then left.[i, j] else right.[i - l1l, j])

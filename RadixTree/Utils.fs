fa6170bb8d97a22af861732e23a8ae0937acfc1c
Florian Biermann <fbie@itu.dk>
2016-02-26 11:23:23 +0100

    get rid of row-column ambiguity
    Just acknowledge order or dimensions.

--- RadixTree/Utils.fs
+++ RadixTree/Utils.fs
@@ -19,30 +19,21 @@ module Array2D =
         arr0.[i, j] <- v
         arr0
 
-    let inline slice arr xmin ymin xmax ymax =
-        Array2D.init (xmax - xmin) (ymax - ymin) (fun x y -> Array2D.get arr (xmin + x) (ymin + y))
-
-    let width = Array2D.length1
-    let height = Array2D.length2
+    let inline slice arr imin jmin imax jmax =
+        Array2D.init (imax - imin) (jmax - jmin) (fun i j -> Array2D.get arr (imin + i) (jmin + j))
 
     (* Chunk the array into subarrays of size n * m. The arrays to the
        right and in the bottom might be smaller if the width and height
        are not multiples of n and m, respectively. *)
     let chunkBySize n m arr =
-        let subArr xmin ymin =
-            let xmax = (min ((xmin + 1) * n) (width arr))
-            let ymax = (min ((ymin + 1) * m) (height arr))
-            slice arr (xmin * n) (ymin * m) xmax ymax
+        let subArr imin jmin =
+            let imax = (min ((imin + 1) * n) (Array2D.length1 arr))
+            let jmax = (min ((jmin + 1) * m) (Array2D.length2 arr))
+            slice arr (imin * n) (jmin * m) imax jmax
         (* TODO: Optimize below. *)
-        let w = if width arr <= n then 1 else width arr / n + 1
-        let h = if height arr <= m then 1 else height arr / m + 1
-        Array2D.init w h subArr
-
-    let inline isSingleColumn arr =
-        width arr = 1
-
-    let inline isSingleRow arr =
-        height arr = 1
+        let l1 = if Array2D.length1 arr <= n then 1 else Array2D.length1 arr / n + 1
+        let l2 = if Array2D.length2 arr <= m then 1 else Array2D.length2 arr / m + 1
+        Array2D.init l1 l2 subArr
 
     let inline isSingleton arr =
         Array2D.length1 arr = 1 && Array2D.length2 arr = 1

38dd67bf069c5976958cb6d13e24e02f2ee6bd02
Florian Biermann <fbie@itu.dk>
2016-03-08 10:55:33 +0100

    extend script to allow easier testing

--- Trees/Script.fsx
+++ Trees/Script.fsx
@@ -1,3 +1,16 @@
 #load "Utils.fs"
 #load "RadixTree2D.fs"
 #load "QuadRope.fs"
+
+open RadTrees
+
+let init = QuadRope.init
+let rows = QuadRope.rows
+let cols = QuadRope.cols
+let get = QuadRope.get
+let split = QuadRope.split
+let hcat = QuadRope.hcat
+let vcat = QuadRope.vcat
+
+let a = init 5 5 (*)
+let b = init 5 10 (*)

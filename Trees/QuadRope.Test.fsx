d85eb4734f6e83cd722ba8f2a505866100250bd9
Florian Biermann <fbie@itu.dk>
2016-03-14 11:41:33 +0100

    generate concatenated ropes also

--- Trees/QuadRope.Test.fsx
+++ Trees/QuadRope.Test.fsx
@@ -24,23 +24,35 @@ let rec maintainsTight = function
 (* Registering QuadRope generator. *)
 module Setup =
 
-    let genRope =
-        gen { let! h = Gen.choose (1, 10)
-              let! w = Gen.choose (1, 10)
-              return QuadRope.init h w (*) }
+    let maxSize = 10
+    let size = Gen.choose (1, maxSize)
 
-    let genWeirdRope =
+    let genRopeOf h w =
+        gen { return QuadRope.init h w (*) }
+
+    let genRope =
+        gen { let! h = size
+              let! w = size
+              return! genRopeOf h w }
+
+    let genRevRope =
         Gen.oneof (seq { yield genRope;
-                         yield gen { let! rope = genRope;
-                                     return QuadRope.hrev rope };
-                         yield gen { let! rope = genRope;
-                                     return QuadRope.vrev rope };
-                         yield gen { let! rope = genRope
-                                     return QuadRope.hrev (QuadRope.vrev rope) }})
+                         yield Gen.map QuadRope.hrev genRope;
+                         yield Gen.map QuadRope.vrev genRope;
+                         yield Gen.map (QuadRope.hrev >> QuadRope.vrev) genRope;
+                         yield Gen.map (QuadRope.vrev >> QuadRope.hrev) genRope })
+
+    let genCatRope =
+        let eq f (a, b) = f a = f b
+        let hs = Gen.oneof (seq { yield Gen.suchThat (eq QuadRope.rows) (Gen.two genRevRope) })
+        let vs = Gen.oneof (seq { yield Gen.suchThat (eq QuadRope.cols) (Gen.two genRevRope) })
+        Gen.oneof ( seq { yield genRevRope;
+                          yield Gen.map (fun (l, r) -> QuadRope.hcat l r) hs;
+                          yield Gen.map (fun (u, l) -> QuadRope.vcat u l) vs })
 
     type QuadRopeGen =
         static member QuadRope () =
-            Arb.fromGen genWeirdRope
+            Arb.fromGen genCatRope
 
     Arb.register<QuadRopeGen>()
 

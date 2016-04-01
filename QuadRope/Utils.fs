951d3433e321b3df590a82f67677cffa208cc3c1
Florian Biermann <fbie@itu.dk>
2016-04-01 14:31:39 +0200

    clean up 2D array utilities
    Add commentary, remove unused functions, rename parameters properly.

--- QuadRope/Utils.fs
+++ QuadRope/Utils.fs
@@ -35,44 +35,43 @@ module Array2D =
     let inline isSingleton arr =
         Array2D.length1 arr = 1 && Array2D.length2 arr = 1
 
-    let append1 bss bs =
-        let l1 = Array2D.length1 bss
-        Array2D.init (l1 + 1) (Array2D.length2 bss) (fun i j -> if i < l1 then bss.[i, j] else Array.get bs j)
-
-    let append2 bss bs =
-        let l2 = Array2D.length2 bss
-        Array2D.init (Array2D.length1 bss) (l2 + 1) (fun i j -> if j < l2 then bss.[i, j] else Array.get bs j)
-
+    (* Concatenate two arrays in first dimension. *)
     let cat1 left right =
         let l1 = Array2D.length1 left + Array2D.length1 right
         let l2 = min (Array2D.length2 left) (Array2D.length2 right)
         let l1l = Array2D.length1 left
         Array2D.init l1 l2 (fun i j -> if i < l1l then left.[i, j] else right.[i - l1l, j])
 
+    (* Concatenate two arrays in second dimension. *)
     let cat2 left right =
         let l1 = min (Array2D.length1 left) (Array2D.length1 right)
         let l2 = Array2D.length2 left + Array2D.length2 right
         let l2l = Array2D.length2 left
         Array2D.init l1 l2 (fun i j -> if j < l2l then left.[i, j] else right.[i, j - l2l])
 
+    (* Revert an array in first dimension. *)
     let rev1 arr =
         let i0 = Array2D.length1 arr - 1
         Array2D.init (Array2D.length1 arr) (Array2D.length2 arr) (fun i j -> arr.[i0 - i, j])
 
+    (* Revert an array in second dimension. *)
     let rev2 arr =
         let j0  = Array2D.length2 arr - 1
         Array2D.init (Array2D.length1 arr) (Array2D.length2 arr) (fun i j -> arr.[i, j0 - j])
 
-    let fold2 f g arr =
-        let fold i _ =
-            Seq.fold f (g i) (seq { for j in 0 .. Array2D.length2 arr - 1 -> arr.[i, j] })
-        Array2D.init (Array2D.length1 arr) 1 fold
-
-    let fold1 f g arr =
+    (* Fold each column of a 2D array, calling state with each row to get the state. *)
+    let fold1 f state arr =
         let fold _ j =
-            Seq.fold f (g j) (seq { for i in 0 .. Array2D.length1 arr - 1 -> arr.[i, j] })
+            Seq.fold f (state j) (seq { for i in 0 .. Array2D.length1 arr - 1 -> arr.[i, j] })
         Array2D.init 1 (Array2D.length2 arr) fold
 
+    (* Fold each column of a 2D array, calling state with each column to get the state. *)
+    let fold2 f state arr =
+        let fold i _ =
+            Seq.fold f (state i) (seq { for j in 0 .. Array2D.length2 arr - 1 -> arr.[i, j] })
+        Array2D.init (Array2D.length1 arr) 1 fold
+
+    (* Initialize a 2D array with all zeros. *)
     let initZeros h w =
         Array2D.init h w (fun _ _ -> 0)
 

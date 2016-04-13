eb623abca9544ea27153d72cf3b53043a849b8a5
Florian Biermann <fbie@itu.dk>
2016-04-13 11:36:26 +0200

    make testing parallel mechanics easier

--- QuadRope/Script.fsx
+++ QuadRope/Script.fsx
@@ -7,3 +7,16 @@
 
 open RadTrees
 open RadTrees.QuadRope
+open RadTrees.QuadRope.Parallel
+
+let mutable calls = 3
+let cond () =
+    if calls <= 0 then
+        true
+    else
+        calls <- (calls - 1)
+        false
+
+let unpack = function
+    | Done rope -> rope, Empty
+    | More (ps, us) -> ps, us

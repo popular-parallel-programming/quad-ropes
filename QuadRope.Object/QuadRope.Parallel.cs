14516ee856a444b71022af95957af2bca42a4a6f
Florian Biermann <fbie@itu.dk>
2016-04-23 14:36:02 +0200

    use VS to automagically implement interface
    All methods throw NotImplementedException.

--- QuadRope.Object/QuadRope.Parallel.cs
+++ QuadRope.Object/QuadRope.Parallel.cs
@@ -10,5 +10,94 @@ namespace RadTrees.QuadRope.Object.Parallel
 	    : base(rope)
 	{ }
 
+        public override IQuadRope<T> BalanceHorizontally()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IQuadRope<T> BalanceVertically()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IQuadRope<T> ConcatHorizontally(IQuadRope<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IQuadRope<T> ConcatVertically(IQuadRope<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IQuadRope<S> FoldHorizontally<S>(Func<S, T, S> f, IQuadRope<S> states)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IQuadRope<S> FoldVertically<S>(Func<S, T, S> f, IQuadRope<S> states)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IQuadRope<S> Map<S>(Func<T, S> f)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IQuadRope<S> MapReduceHorizontally<S>(Func<T, S> f, Func<S, S, S> g)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IQuadRope<S> MapReduceVertically<S>(Func<T, S> f, Func<S, S, S> g)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IQuadRope<S> ReduceHorizontally<S>(Func<S, S, S> f)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IQuadRope<S> ReduceVertically<S>(Func<S, S, S> f)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IQuadRope<T> ReverseHorizontally()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IQuadRope<T> ReverseVertically()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IQuadRope<T> ScanHorizontally(Func<T, T, T> f, Func<int, T> states)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IQuadRope<T> ScanVertically(Func<T, T, T> f, Func<int, T> states)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IQuadRope<T> Set(int row, int col, T v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IQuadRope<T> Slice(int row, int col, int rows, int cols)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IQuadRope<T> Transpose()
+        {
+            throw new NotImplementedException();
+        }
     }
 }

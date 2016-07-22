bfe0c089619ef2b18064ee05333ee745ff31048d
Florian Biermann <fbie@itu.dk>
2016-07-22 13:56:08 +0800

    clean up script file

--- QuadRope/Script.fsx
+++ QuadRope/Script.fsx
@@ -1,27 +1,11 @@
 #load "Types.fs"
 #load "Utils.fs"
-#load "Array2DView.fs"
+#load "Array2D.fs"
+#load "Array2D.Parallel.fs"
 #load "QuadRope.fs"
 #load "QuadRope.Parallel.fs"
+#load "Examples.fs"
 
 #nowarn "62"
 
 open RadTrees
-
-let rope = QuadRope.init 10 10 (*)
-let a = QuadRope.init 4 4 (*);
-let b = QuadRope.hcat a a;;
-let c = QuadRope.vcat a a;;
-
-let rec dontimes n x f =
-    if n = 0 then
-        x
-    else
-        dontimes (n - 1) (f x) f
-
-let hcatn n =
-    let q = QuadRope.initZeros 1 1
-    let mutable p = q
-    for i in 1 .. n do
-        p <- QuadRope.hcat p q
-    p

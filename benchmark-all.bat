095694ddf00020dad46fb2a64559f50c29dd3efa
Florian Biermann <fbie@itu.dk>
2016-08-23 15:44:37 +0800

    use call to avoid terminating outer script

--- benchmark-all.bat
+++ benchmark-all.bat
@@ -1,51 +1,51 @@
 :: Scaling benchmarks for single functions
-benchmark       > logs\benchmark-s1000-t01.txt
-benchmark -t 2  > logs\benchmark-s1000-t02.txt
-benchmark -t 3  > logs\benchmark-s1000-t03.txt
-benchmark -t 4  > logs\benchmark-s1000-t04.txt
-benchmark -t 5  > logs\benchmark-s1000-t05.txt
-benchmark -t 6  > logs\benchmark-s1000-t06.txt
-benchmark -t 7  > logs\benchmark-s1000-t07.txt
-benchmark -t 8  > logs\benchmark-s1000-t08.txt
-benchmark -t 9  > logs\benchmark-s1000-t09.txt
-benchmark -t 10 > logs\benchmark-s1000-t10.txt
-benchmark -t 11 > logs\benchmark-s1000-t11.txt
-benchmark -t 12 > logs\benchmark-s1000-t12.txt
-benchmark -t 13 > logs\benchmark-s1000-t13.txt
-benchmark -t 14 > logs\benchmark-s1000-t14.txt
-benchmark -t 15 > logs\benchmark-s1000-t15.txt
-benchmark -t 16 > logs\benchmark-s1000-t16.txt
+call benchmark       > logs\benchmark-s1000-t01.txt
+call benchmark -t 2  > logs\benchmark-s1000-t02.txt
+call benchmark -t 3  > logs\benchmark-s1000-t03.txt
+call benchmark -t 4  > logs\benchmark-s1000-t04.txt
+call benchmark -t 5  > logs\benchmark-s1000-t05.txt
+call benchmark -t 6  > logs\benchmark-s1000-t06.txt
+call benchmark -t 7  > logs\benchmark-s1000-t07.txt
+call benchmark -t 8  > logs\benchmark-s1000-t08.txt
+call benchmark -t 9  > logs\benchmark-s1000-t09.txt
+call benchmark -t 10 > logs\benchmark-s1000-t10.txt
+call benchmark -t 11 > logs\benchmark-s1000-t11.txt
+call benchmark -t 12 > logs\benchmark-s1000-t12.txt
+call benchmark -t 13 > logs\benchmark-s1000-t13.txt
+call benchmark -t 14 > logs\benchmark-s1000-t14.txt
+call benchmark -t 15 > logs\benchmark-s1000-t15.txt
+call benchmark -t 16 > logs\benchmark-s1000-t16.txt
 
 :: Scaling benchmarks for primes
-benchmark -m primes -t 2  > logs\benchmark-primes-s1000-t02.txt
-benchmark -m primes -t 3  > logs\benchmark-primes-s1000-t03.txt
-benchmark -m primes -t 4  > logs\benchmark-primes-s1000-t04.txt
-benchmark -m primes -t 5  > logs\benchmark-primes-s1000-t05.txt
-benchmark -m primes -t 6  > logs\benchmark-primes-s1000-t06.txt
-benchmark -m primes -t 7  > logs\benchmark-primes-s1000-t07.txt
-benchmark -m primes -t 8  > logs\benchmark-primes-s1000-t08.txt
-benchmark -m primes -t 9  > logs\benchmark-primes-s1000-t09.txt
-benchmark -m primes -t 10 > logs\benchmark-primes-s1000-t10.txt
-benchmark -m primes -t 11 > logs\benchmark-primes-s1000-t11.txt
-benchmark -m primes -t 12 > logs\benchmark-primes-s1000-t12.txt
-benchmark -m primes -t 13 > logs\benchmark-primes-s1000-t13.txt
-benchmark -m primes -t 14 > logs\benchmark-primes-s1000-t14.txt
-benchmark -m primes -t 15 > logs\benchmark-primes-s1000-t15.txt
-benchmark -m primes -t 16 > logs\benchmark-primes-s1000-t16.txt
+call benchmark -m primes -s 100 -t 2  > logs\benchmark-primes-s100-t02.txt
+call benchmark -m primes -s 100 -t 3  > logs\benchmark-primes-s100-t03.txt
+call benchmark -m primes -s 100 -t 4  > logs\benchmark-primes-s100-t04.txt
+call benchmark -m primes -s 100 -t 5  > logs\benchmark-primes-s100-t05.txt
+call benchmark -m primes -s 100 -t 6  > logs\benchmark-primes-s100-t06.txt
+call benchmark -m primes -s 100 -t 7  > logs\benchmark-primes-s100-t07.txt
+call benchmark -m primes -s 100 -t 8  > logs\benchmark-primes-s100-t08.txt
+call benchmark -m primes -s 100 -t 9  > logs\benchmark-primes-s100-t09.txt
+call benchmark -m primes -s 100 -t 10 > logs\benchmark-primes-s100-t10.txt
+call benchmark -m primes -s 100 -t 11 > logs\benchmark-primes-s100-t11.txt
+call benchmark -m primes -s 100 -t 12 > logs\benchmark-primes-s100-t12.txt
+call benchmark -m primes -s 100 -t 13 > logs\benchmark-primes-s100-t13.txt
+call benchmark -m primes -s 100 -t 14 > logs\benchmark-primes-s100-t14.txt
+call benchmark -m primes -s 100 -t 15 > logs\benchmark-primes-s100-t15.txt
+call benchmark -m primes -s 100 -t 16 > logs\benchmark-primes-s100-t16.txt
 
 :: Scaling benchmarks for mmult
-benchmark -m mmult -t 2  > logs\benchmark-mmult-s1000-t02.txt
-benchmark -m mmult -t 3  > logs\benchmark-mmult-s1000-t03.txt
-benchmark -m mmult -t 4  > logs\benchmark-mmult-s1000-t04.txt
-benchmark -m mmult -t 5  > logs\benchmark-mmult-s1000-t05.txt
-benchmark -m mmult -t 6  > logs\benchmark-mmult-s1000-t06.txt
-benchmark -m mmult -t 7  > logs\benchmark-mmult-s1000-t07.txt
-benchmark -m mmult -t 8  > logs\benchmark-mmult-s1000-t08.txt
-benchmark -m mmult -t 9  > logs\benchmark-mmult-s1000-t09.txt
-benchmark -m mmult -t 10 > logs\benchmark-mmult-s1000-t10.txt
-benchmark -m mmult -t 11 > logs\benchmark-mmult-s1000-t11.txt
-benchmark -m mmult -t 12 > logs\benchmark-mmult-s1000-t12.txt
-benchmark -m mmult -t 13 > logs\benchmark-mmult-s1000-t13.txt
-benchmark -m mmult -t 14 > logs\benchmark-mmult-s1000-t14.txt
-benchmark -m mmult -t 15 > logs\benchmark-mmult-s1000-t15.txt
-benchmark -m mmult -t 16 > logs\benchmark-mmult-s1000-t16.txt
+call benchmark -m mmult -s 40 -t 2  > logs\benchmark-mmult-s40-t02.txt
+call benchmark -m mmult -s 40 -t 3  > logs\benchmark-mmult-s40-t03.txt
+call benchmark -m mmult -s 40 -t 4  > logs\benchmark-mmult-s40-t04.txt
+call benchmark -m mmult -s 40 -t 5  > logs\benchmark-mmult-s40-t05.txt
+call benchmark -m mmult -s 40 -t 6  > logs\benchmark-mmult-s40-t06.txt
+call benchmark -m mmult -s 40 -t 7  > logs\benchmark-mmult-s40-t07.txt
+call benchmark -m mmult -s 40 -t 8  > logs\benchmark-mmult-s40-t08.txt
+call benchmark -m mmult -s 40 -t 9  > logs\benchmark-mmult-s40-t09.txt
+call benchmark -m mmult -s 40 -t 10 > logs\benchmark-mmult-s40-t10.txt
+call benchmark -m mmult -s 40 -t 11 > logs\benchmark-mmult-s40-t11.txt
+call benchmark -m mmult -s 40 -t 12 > logs\benchmark-mmult-s40-t12.txt
+call benchmark -m mmult -s 40 -t 13 > logs\benchmark-mmult-s40-t13.txt
+call benchmark -m mmult -s 40 -t 14 > logs\benchmark-mmult-s40-t14.txt
+call benchmark -m mmult -s 40 -t 15 > logs\benchmark-mmult-s40-t15.txt
+call benchmark -m mmult -s 40 -t 16 > logs\benchmark-mmult-s40-t16.txt

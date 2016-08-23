a1af9d53739d8a757039715cf41edd865a2d3284
Florian Biermann <fbie@itu.dk>
2016-08-23 16:18:17 +0800

    adjust sizes

--- benchmark-all.bat
+++ benchmark-all.bat
@@ -34,18 +34,19 @@ call benchmark -m primes -s 100 -t 15 > logs\benchmark-primes-s100-t15.txt
 call benchmark -m primes -s 100 -t 16 > logs\benchmark-primes-s100-t16.txt
 
 :: Scaling benchmarks for mmult
-call benchmark -m mmult -s 40 -t 2  > logs\benchmark-mmult-s40-t02.txt
-call benchmark -m mmult -s 40 -t 3  > logs\benchmark-mmult-s40-t03.txt
-call benchmark -m mmult -s 40 -t 4  > logs\benchmark-mmult-s40-t04.txt
-call benchmark -m mmult -s 40 -t 5  > logs\benchmark-mmult-s40-t05.txt
-call benchmark -m mmult -s 40 -t 6  > logs\benchmark-mmult-s40-t06.txt
-call benchmark -m mmult -s 40 -t 7  > logs\benchmark-mmult-s40-t07.txt
-call benchmark -m mmult -s 40 -t 8  > logs\benchmark-mmult-s40-t08.txt
-call benchmark -m mmult -s 40 -t 9  > logs\benchmark-mmult-s40-t09.txt
-call benchmark -m mmult -s 40 -t 10 > logs\benchmark-mmult-s40-t10.txt
-call benchmark -m mmult -s 40 -t 11 > logs\benchmark-mmult-s40-t11.txt
-call benchmark -m mmult -s 40 -t 12 > logs\benchmark-mmult-s40-t12.txt
-call benchmark -m mmult -s 40 -t 13 > logs\benchmark-mmult-s40-t13.txt
-call benchmark -m mmult -s 40 -t 14 > logs\benchmark-mmult-s40-t14.txt
-call benchmark -m mmult -s 40 -t 15 > logs\benchmark-mmult-s40-t15.txt
-call benchmark -m mmult -s 40 -t 16 > logs\benchmark-mmult-s40-t16.txt
+call benchmark -m mmult -s 200 -t 1  > logs\benchmark-mmult-s200-t01.txt
+call benchmark -m mmult -s 200 -t 2  > logs\benchmark-mmult-s200-t02.txt
+call benchmark -m mmult -s 200 -t 3  > logs\benchmark-mmult-s200-t03.txt
+call benchmark -m mmult -s 200 -t 4  > logs\benchmark-mmult-s200-t04.txt
+call benchmark -m mmult -s 200 -t 5  > logs\benchmark-mmult-s200-t05.txt
+call benchmark -m mmult -s 200 -t 6  > logs\benchmark-mmult-s200-t06.txt
+call benchmark -m mmult -s 200 -t 7  > logs\benchmark-mmult-s200-t07.txt
+call benchmark -m mmult -s 200 -t 8  > logs\benchmark-mmult-s200-t08.txt
+call benchmark -m mmult -s 200 -t 9  > logs\benchmark-mmult-s200-t09.txt
+call benchmark -m mmult -s 200 -t 10 > logs\benchmark-mmult-s200-t10.txt
+call benchmark -m mmult -s 200 -t 11 > logs\benchmark-mmult-s200-t11.txt
+call benchmark -m mmult -s 200 -t 12 > logs\benchmark-mmult-s200-t12.txt
+call benchmark -m mmult -s 200 -t 13 > logs\benchmark-mmult-s200-t13.txt
+call benchmark -m mmult -s 200 -t 14 > logs\benchmark-mmult-s200-t14.txt
+call benchmark -m mmult -s 200 -t 15 > logs\benchmark-mmult-s200-t15.txt
+call benchmark -m mmult -s 200 -t 16 > logs\benchmark-mmult-s200-t16.txt

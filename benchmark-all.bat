42cf1165009a3c1b8840faf5258e22dd64367968
Florian Biermann <fbie@itu.dk>
2016-08-24 10:44:12 +0800

    only call scaling benchmarks once

--- benchmark-all.bat
+++ benchmark-all.bat
@@ -17,36 +17,7 @@ call benchmark -t 15 > logs\benchmark-s1000-t15.txt
 call benchmark -t 16 > logs\benchmark-s1000-t16.txt
 
 :: Scaling benchmarks for primes
-call benchmark -m primes -s 100 -t 2  > logs\benchmark-primes-s100-t02.txt
-call benchmark -m primes -s 100 -t 3  > logs\benchmark-primes-s100-t03.txt
-call benchmark -m primes -s 100 -t 4  > logs\benchmark-primes-s100-t04.txt
-call benchmark -m primes -s 100 -t 5  > logs\benchmark-primes-s100-t05.txt
-call benchmark -m primes -s 100 -t 6  > logs\benchmark-primes-s100-t06.txt
-call benchmark -m primes -s 100 -t 7  > logs\benchmark-primes-s100-t07.txt
-call benchmark -m primes -s 100 -t 8  > logs\benchmark-primes-s100-t08.txt
-call benchmark -m primes -s 100 -t 9  > logs\benchmark-primes-s100-t09.txt
-call benchmark -m primes -s 100 -t 10 > logs\benchmark-primes-s100-t10.txt
-call benchmark -m primes -s 100 -t 11 > logs\benchmark-primes-s100-t11.txt
-call benchmark -m primes -s 100 -t 12 > logs\benchmark-primes-s100-t12.txt
-call benchmark -m primes -s 100 -t 13 > logs\benchmark-primes-s100-t13.txt
-call benchmark -m primes -s 100 -t 14 > logs\benchmark-primes-s100-t14.txt
-call benchmark -m primes -s 100 -t 15 > logs\benchmark-primes-s100-t15.txt
 call benchmark -m primes -s 100 -t 16 > logs\benchmark-primes-s100-t16.txt
 
 :: Scaling benchmarks for mmult
-call benchmark -m mmult -s 200 -t 1  > logs\benchmark-mmult-s200-t01.txt
-call benchmark -m mmult -s 200 -t 2  > logs\benchmark-mmult-s200-t02.txt
-call benchmark -m mmult -s 200 -t 3  > logs\benchmark-mmult-s200-t03.txt
-call benchmark -m mmult -s 200 -t 4  > logs\benchmark-mmult-s200-t04.txt
-call benchmark -m mmult -s 200 -t 5  > logs\benchmark-mmult-s200-t05.txt
-call benchmark -m mmult -s 200 -t 6  > logs\benchmark-mmult-s200-t06.txt
-call benchmark -m mmult -s 200 -t 7  > logs\benchmark-mmult-s200-t07.txt
-call benchmark -m mmult -s 200 -t 8  > logs\benchmark-mmult-s200-t08.txt
-call benchmark -m mmult -s 200 -t 9  > logs\benchmark-mmult-s200-t09.txt
-call benchmark -m mmult -s 200 -t 10 > logs\benchmark-mmult-s200-t10.txt
-call benchmark -m mmult -s 200 -t 11 > logs\benchmark-mmult-s200-t11.txt
-call benchmark -m mmult -s 200 -t 12 > logs\benchmark-mmult-s200-t12.txt
-call benchmark -m mmult -s 200 -t 13 > logs\benchmark-mmult-s200-t13.txt
-call benchmark -m mmult -s 200 -t 14 > logs\benchmark-mmult-s200-t14.txt
-call benchmark -m mmult -s 200 -t 15 > logs\benchmark-mmult-s200-t15.txt
-call benchmark -m mmult -s 200 -t 16 > logs\benchmark-mmult-s200-t16.txt
+call benchmark -m mmult -s 200 -t 16 > logs\benchmark-mmult-s200-t01.txt

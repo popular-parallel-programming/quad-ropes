71b3deea77c2af5d364c8a6009c97e3009c2f11d
Florian Biermann <fbie@itu.dk>
2016-11-07 18:20:03 +0800

    re-implement Target type as struct
    Structs are value-types and stay on the stack.

--- QuadRope/Target.fs
+++ QuadRope/Target.fs
@@ -27,65 +27,72 @@ open Types
 
 /// A convenience wrapper for writing into a target array with
 /// some offset.
-type 'a Target = { i : int; j : int; vals : 'a [,] }
+type 'a Target =
+    struct
+        val i : int
+        val j : int
+        val vals : 'a [,]
+        new(i : int, j : int, vals : 'a[,]) = { i = i; j = j; vals = vals }
+    end
 
 /// Create a new target descriptor of size h * w.
-let make h w =
-    { i = 0; j = 0; vals = Array2D.zeroCreate h w }
+let inline make h w = Target(0, 0, Array2D.zeroCreate h w)
 
-/// Only indices, array is null. This is useful for e.g. iteri
-let indices =
-    { i = 0; j = 0; vals = null }
+let inline increment (tgt : _ Target) i j =
+    Target(tgt.i + i, tgt.j + j, tgt.vals)
+
+let inline incrementRow tgt i = increment tgt i 0
+let inline incrementCol tgt j = increment tgt 0 j
 
 /// Adjust target descriptor to match north-eastern child.
 let ne tgt = function
     | Node (_, _, _, _, nw, _, _) ->
-        { tgt with j = tgt.j + cols nw }
+        incrementCol tgt (cols nw)
     | _ -> tgt
 
 /// Adjust target descriptor to match south-western child.
 let sw tgt = function
     | Node (_, _, _, _, nw, _, _) ->
-        { tgt with i = tgt.i + rows nw }
+        incrementRow tgt (rows nw)
     | _ -> tgt
 
 /// Adjust target descriptor to match south-eastern child.
 let se tgt = function
     | Node (_, _, _, ne, _, sw, _) ->
-        { tgt with i = tgt.i + rows ne; j = tgt.j + cols sw}
+        increment tgt (rows ne) (cols sw)
     | _ -> tgt
 
 /// Generalized write to target.
-let writemap tgt f r c v =
+let writemap (tgt : _ Target) f r c v =
     tgt.vals.[tgt.i + r, tgt.j + c] <- f v
 
 /// Generalized write to target with index pairs.
-let writemapi tgt f r c v =
+let writemapi (tgt : _ Target) f r c v =
     let i = tgt.i + r
     let j = tgt.j + c
     tgt.vals.[i, j] <- f i j v
 
 /// Simplified write to target.
-let write tgt r c v =
+let write (tgt : _ Target) r c v =
     writemap tgt id r c v
 
 /// Build a leaf node from a target for a given height and width.
-let toSlice tgt h w =
+let toSlice (tgt : _ Target) h w =
     ArraySlice.makeSlice tgt.i tgt.j h w tgt.vals
 
 /// Map a function to the values of a leaf and return a new leaf
 /// instance. This writes to the underlying target array.
-let map f vals tgt =
+let map f vals (tgt : _ Target) =
     ArraySlice.iteri (writemap tgt f) vals
     toSlice tgt (ArraySlice.length1 vals) (ArraySlice.length2 vals)
 
 /// Map a function to the values and index pairs of a leaf and
 /// return a new leaf instance. This writes to the underlying
 /// target array.
-let mapi f vals tgt =
+let mapi f vals (tgt : _ Target) =
     ArraySlice.iteri (writemapi tgt f) vals
     toSlice tgt (ArraySlice.length1 vals) (ArraySlice.length2 vals)
 
 /// Use the offset stored in tgt to iterate over some array slice.
-let iteri f vals tgt =
+let iteri f vals (tgt : _ Target) =
     ArraySlice.iteri (fun i j v -> f (tgt.i + i) (tgt.j + j) v) vals

d413faaf32c669f99ed0bc8e257ce9ff88708066
Florian Biermann <fbie@itu.dk>
2017-01-19 14:33:15 +0100

    separate generator registration from test runs visually
    To keep me from commenting registration out - the results are devastating!

--- QuadRope.Test/Test.fs
+++ QuadRope.Test/Test.fs
@@ -25,12 +25,14 @@ module TestRunner =
     open FsCheck
 
     let test () =
-        RadTrees.Test.Gen.register() |> ignore
+        // Register generators.
+        Gen.register() |> ignore
+
+        // Run tests.
         Check.QuickAll (typeof<Utils.Handle>.DeclaringType)
         Check.QuickAll (typeof<QuadRope.Handle>.DeclaringType)
         Check.QuickAll (typeof<Parallel.QuadRope.Handle>.DeclaringType)
         Check.QuickAll (typeof<Interesting.QuadRope.Handle>.DeclaringType)
-
     [<EntryPoint>]
     let main _ =
         try

e727db050f7c8d42b7938d7d35daea7e5b1159b5
Florian Biermann <fbie@itu.dk>
2017-03-24 10:43:20 +0800

    remove unused experiment code

--- QuadRope/Script.fsx
+++ QuadRope/Script.fsx
@@ -6,7 +6,6 @@
 #load "Target.fs"
 #load "QuadRope.fs"
 #load "QuadRope.Parallel.fs"
-#load "Examples.fs"
 
 #nowarn "62"
 
@@ -56,42 +55,5 @@ let adversarial =
 
 let q = adversarial (QuadRope.init 1 1 (+)) 10
 
-
-// Amino acid sequence alignment on quad ropes.
-let strlen = String.length
-
-/// Compute the max of a and b by means of f.
-let maxBy f a b = if f a > f b then a else b
-
-/// Find the maximum value in scores and return its index.
-let rec findMax scores =
-    QuadRope.mapi (fun i j s -> (i, j), s) scores
-    |> QuadRope.reduce (maxBy snd) ((0, 0), 0)
-    |> fst
-
-/// Find the next highest index during backtracking.
-let findNextHeigest i j scores =
-    let scores' = QuadRope.set (QuadRope.slice (i - 1) (j - 1) 2 2 scores) 1 1 -1
-    let i', j' = findMax scores'
-    i - 1 + i', j - 1 + j'
-
-/// Backtrack through a score matrix from some starting index pair i
-/// and j.
-let rec backtrace (i, j) scores trace =
-    let score = QuadRope.get scores i j
-    let trace' = trace + score
-    if score = 0 then
-        trace'
-    else
-        backtrace (findNextHighest i j scores) scores trace'
-
-/// Compute the score after Smith-Waterman.
-let swscore row diag col s =
-    max (max (diag + s) (max (row - 1) (col - 1))) 0
-
-/// Compute the alignment cost of two sequences a and b.
-let align a b =
-    let scores =
-        QuadRope.init (strlen a) (strlen b) (fun i j -> if a.[i] = b.[j] then 1 else - 1)
-        |> QuadRope.scan swscore 0
-    backtrace (findMax scores) scores 0
+let q0 = QuadRope.init 4 4 (*)
+let q1 = dontimes 10 (fun _ q -> QuadRope.hcat q0 q) q0
